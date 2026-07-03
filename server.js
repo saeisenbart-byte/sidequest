c5aab73c0dd4a22c5ef93d9b9407c92e11f63721
Sophia Eisenbart Macias <saeisenbart@gmail.com>
2026-07-03 11:40:10 -0500

    Use Render port

--- server.js
+++ server.js
@@ -206,7 +206,7 @@ quests.forEach((quest) => {
     );
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
